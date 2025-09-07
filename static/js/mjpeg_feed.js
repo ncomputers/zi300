cece819ca65d5e620a7f94be02e49b493329992a
ncomputers <ncomputerudr@gmail.com>
2025-09-07 19:43:43 +0530

    Revert "Fix MJPEG streaming and dashboard feeds"

--- static/js/mjpeg_feed.js
+++ static/js/mjpeg_feed.js
@@ -1,8 +1,26 @@
 (function(){
-  function initMjpegFeeds(root=document){
-    // leave existing <img> src untouched; template sets /api/cameras/{id}/mjpeg
+  function startFeed(img){
+    const cam = img.dataset.cam;
+    if(!cam) return;
+    fetch(`/api/cameras/${cam}/show`,{method:'POST'}).catch(e=>console.error('show',e));
+    img.src = `/api/cameras/${cam}/mjpeg`;
+    const modal = img.closest('.modal');
+    if(modal){
+      modal.addEventListener('hidden.bs.modal',()=>{
+        fetch(`/api/cameras/${cam}/hide`,{method:'POST'}).catch(e=>console.error('hide',e));
+        img.removeAttribute('src');
+      },{once:true});
+    }
   }
-  if (typeof module !== "undefined") module.exports = { initMjpegFeeds };
-  if (typeof document !== "undefined" && !globalThis.__TEST__) initMjpegFeeds();
+  function initMjpegFeeds(root=document){
+    root.querySelectorAll('img.feed-img').forEach(startFeed);
+  }
+  if(typeof module!=='undefined'){
+    module.exports={initMjpegFeeds};
+  }else{
+    globalThis.initMjpegFeeds=initMjpegFeeds;
+  }
+  if(typeof document!=='undefined' && !globalThis.__TEST__){
+    initMjpegFeeds();
+  }
 })();
-
--- tests/mjpeg_feed.test.js
+++ tests/mjpeg_feed.test.js
@@ -2,13 +2,23 @@
  * @jest-environment jsdom
  */
 
-test('leaves existing src intact', () => {
-  document.body.innerHTML = '<img class="feed-img" src="/api/cameras/1/mjpeg">';
-  globalThis.__TEST__ = true;
-  const { initMjpegFeeds } = require('../static/js/mjpeg_feed.js');
-  const img = document.querySelector('img.feed-img');
+test('calls show/hide endpoints and clears src', () => {
+  document.body.innerHTML = '<div class="modal"><img class="feed-img" data-cam="1"></div>';
   global.fetch = jest.fn(() => Promise.resolve());
-  initMjpegFeeds(document);
-  expect(fetch).not.toHaveBeenCalled();
+
+  globalThis.__TEST__ = true;
+  const fetchMock = jest.fn(() => Promise.resolve({}));
+  global.fetch = fetchMock;
+  const fs = require('fs');
+  const path = require('path');
+  const code = fs.readFileSync(path.resolve(__dirname, '../static/js/mjpeg_feed.js'), 'utf8');
+  new Function(code)();
+  globalThis.initMjpegFeeds(document);
+  const modal = document.querySelector('.modal');
+  const img = document.querySelector('img.feed-img');
+  expect(fetch).toHaveBeenCalledWith('/api/cameras/1/show', {method: 'POST'});
   expect(img.getAttribute('src')).toBe('/api/cameras/1/mjpeg');
+  document.querySelector('.modal').dispatchEvent(new Event('hidden.bs.modal'));
+  expect(fetch).toHaveBeenCalledWith('/api/cameras/1/hide', {method: 'POST'});
+  expect(img.getAttribute('src')).toBeNull();
 });
